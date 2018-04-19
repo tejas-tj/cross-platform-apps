cc525c2a2bdeecf6df0a1f1c3921a4f42082b1ff
pnarasim <priya.narasimhan@gmail.com>
2018-04-19 18:36:40 +0530

    changes to details page to read and write alll values properly. pls see the details.ts file for the format of the buffer written to the device.
    Ref Prithvi: we have decided to use only 2 chars. a UUID_SENSE_BOARD_SETTINGS which will read
    things like firmware version, board revision/manuf details etc.
    and a UUID_SENSE_PI_USER_SETTINGS to write all the users settings to 1 charateristic. (11 bytes as of now)
    (the format/order of the bytes specified in details.ts)
    
    eventually we shd check the settings we read and then write if compatible. as of now, not reading.
    and even more eventually :) we shd actually determine what settings can be modified by reading the settings from
    the board.
    
    current issues:
    1. i get an error 13 when writing : could be an issue on the board side : not letting me write 11 bytes. to be checked.
    2. the scannign takes much too long. even now, sometimes 1-2 minutes before the sensepi is discovered by the app. check the scan settings

--- bluetoothConfigurator/src/myApp/src/pages/detail/detail.ts
+++ bluetoothConfigurator/src/myApp/src/pages/detail/detail.ts
@@ -4,24 +4,54 @@ import { BLE } from '@ionic-native/ble';
 
 // Bluetooth UUIDs
 const UUID_SENSE_PI_SERVICE = '3c73dc50-07f5-480d-b066-837407fbde0a';
-const UUID_SENSE_PI_TIME = '3c73dc51-07f5-480d-b066-837407fbde0a';
-                               
-const UUID_SENSE_PI_INTERFACE_VERSION = '3c73dc51-07f5-480d-b066-837407fbde0a';
+const UUID_SENSE_BOARD_SETTINGS = '3c73dc51-07f5-480d-b066-837407fbde0a';
+const UUID_SENSE_PI_USER_SETTINGS = '3c73dc52-07f5-480d-b066-837407fbde0a';
 
-/*
-const UUID_SENSE_PI_TIME = '3c73dc51-07f5-480d-b066-837407fbde0a';
-const UUID_SENSE_PI_MODE = '3c73dc52-07f5-480d-b066-837407fbde0a';
-const UUID_SENSE_PI_SENSITIVITY = '3c73dc53-07f5-480d-b066-837407fbde0a';
-const UUID_SENSE_PI_TRIGGERS = '3c73dc54-07f5-480d-b066-837407fbde0a';
-const UUID_SENSE_PI_ACTIVATE = '3c73dc55-07f5-480d-b066-837407fbde0a';
-const UUID_SENSE_PI_CAMERA = '3c73dc56-07f5-480d-b066-837407fbde0a';
-*/
+const FW_VER='1.0';
 
+const SENSEPI_SETTINGS_LENGTH=11;
+const OFFSET_TIME=0;
+const OFFSET_MODE=1;
+const OFFSET_BURSTGAP_BULBEXP=2;
+const OFFSET_VIDEO=4;
+const OFFSET_SENSITIVITY=5;
+const OFFSET_TRIGGER_GAP=6;
+const OFFSET_FOCUS_ACTIVATED=8;
+const OFFSET_MAKE=9;
+const OFFSET_MODEL=10;
+
+/* not needed now
 enum TIME_SETTING {
   NIGHT_ONLY,
   DAY_ONLY,
   DAYNIGHT_BOTH 
 }
+*/
+
+//pnarasim : want to use this type throughout, but not able to pass to the html
+//this is a placeholder till i figure that out
+/*
+type SensePiSettings  = {
+  timeSetting: number;
+  mode: number;
+  burstGap: number;
+  bulbExposureTime: number;
+  videoDuration: number;
+  videoExtension: number;
+  sensitivity: number;
+  triggerGap: number;
+  focusActivated: boolean;
+}
+*/
+
+enum MODE_SETTING {
+  TRIGGER_SINGLE,
+  TRIGGER_BURST,
+  TRIGGER_BULB_EXPOSURE,
+  TRIGGER_VIDEO,
+  TRIGGER_FOCUS  
+}
+
 
 @Component({
   selector: 'page-detail',
@@ -31,32 +61,33 @@ export class DetailPage {
   
   peripheral: any = {};
     
-  timeSetting: Uint8Array;
+  //settings : SensePiSettings;
 
+  timeSetting: number;
   mode: number;
   sensitivity: number;
-  triggers: number;
-  isActivated: number;
+  triggerGap: number;
+  focusActivated: boolean = false;
 
   
-  burstTime: number;
-  exposureTime: number;
+  burstGap: number;
+  bulbExposureTime: number;
   videoDuration: number;
   videoExtension: number;
   
-  buttonClickedSingle: boolean = false;
-  buttonClickedBurst: boolean = false;
-  buttonClickedBulb: boolean = false;
-  buttonClickedVideo: boolean = false;
-  buttonClickedFocus: boolean = false;
+  radioClickedSingle: boolean = false;
+  radioClickedBurst: boolean = false;
+  radioClickedBulb: boolean = false;
+  radioClickedVideo: boolean = false;
+  radioClickedFocus: boolean = false;
 
   public buttonColor: string = "plain";
     
-  companies: any[];
+  makes: any[];
   models: any[];
   selectedModels: any[];
   
-  company: any;
+  make: any;
   model: any;
   
   statusMessage: string;
@@ -98,8 +129,8 @@ export class DetailPage {
         
       );
 
-      this.initializeCompany();
-      this.initializeModel();
+      this.initializeMakes();
+      this.initializeModels();
       
     }
     
@@ -111,7 +142,7 @@ export class DetailPage {
       this.setStatus('Connected to ' + (peripheral.name || peripheral.id));
       console.log(JSON.stringify(peripheral, null, 2));
       //pnarasim : why this?
-      /*this.ble.startNotification(this.peripheral.id, UUID_SENSE_PI_SERVICE, UUID_SENSE_PI_TIME).subscribe(
+      /*this.ble.startNotification(this.peripheral.id, UUID_SENSE_PI_SERVICE, UUID_SENSE_PI_USER_SETTINGS).subscribe(
         () => this.showAlert('Unexpected Error', 'Failed to subscribe')
       )*/
     }
@@ -119,70 +150,95 @@ export class DetailPage {
 
     public setTimeSetting(event) {
       console.log('timeSetting : time was set to ' + event);
-      this.timeSetting = new Uint8Array(4);
-      this.timeSetting[0]=0x00;
-      this.timeSetting[1]=0x00;
-      this.timeSetting[2]=0x00;
-      this.timeSetting[3]=event;
+      this.timeSetting = event;
     }
     
-    /* pnarasim: unused
-    public selectMode(event) {
-            
-    }
-    */
-
-    public setSingleTrigger(event) {
-      
-    }
-    
-    public unsetSingleTrigger(event) {
-      
-    }
-    
-    // To show input box to enter time between bursts
-    public onButtonClickBurst() {
-      this.buttonClickedBurst = !this.buttonClickedBurst;
-    }
-    
-    // To show input box to enter time for exposure at night
-    public onButtonClickBulb() {
-      this.buttonClickedBulb = !this.buttonClickedBulb;
-    }
-    
-    // To show input box to enter duration of video & extension time
-    public onButtonClickVideo() {
-      this.buttonClickedVideo = !this.buttonClickedVideo;
+    public resetModes() {
+      this.radioClickedSingle = false;
+      this.radioClickedBurst = false;
+      this.radioClickedBulb = false;
+      this.radioClickedVideo = false;
+      this.radioClickedFocus = false;
     }
 
-    public onButtonClickFocus() {
-      
+    public setBurstGap(event) {
+      console.log("Burst Gap is set to " + this.burstGap);
+    }
+
+    public setBulbExposureTime(event) {
+      console.log("Bulb Exposure Time is set to " + this.bulbExposureTime);
+    }
+
+    public setVideoDuration(event) {
+       console.log("Video Duration is set to " + this.videoDuration);
+    }
+
+    public setVideoExtension(event) {
+       console.log("Video Extension is set to " + this.videoExtension);
+    }
+
+    public setMode(event) {
+      console.log('mode : mode selected was ' + event);
+      //everytime user selects a different mode, reset all to zero.
+      this.resetModes();
+      this.mode = event;
+      switch (+event) {
+        case MODE_SETTING.TRIGGER_SINGLE: {
+          console.log('Radio button SINGLE TRIGGER selected');
+          this.radioClickedSingle = true;
+          break;
+        } 
+        case MODE_SETTING.TRIGGER_BURST:{
+          console.log('Radio button BURST TRIGGER selected');
+          this.radioClickedBurst = true;
+          console.log('Burst Gap selected as ' + this.burstGap);
+          break;
+        }
+        case MODE_SETTING.TRIGGER_BULB_EXPOSURE: {
+          console.log('Radio button BULB EXPPOSURE TRIGGER selected');
+          this.radioClickedBulb = true;
+          console.log('bulbExposureTime set to ' + this.bulbExposureTime);
+          break;
+        }
+        case MODE_SETTING.TRIGGER_VIDEO: {
+          console.log('Radio button VIDEO TRIGGER selected');
+          this.radioClickedVideo = true;
+          console.log('videoDuration = ' + this.videoDuration + ' videoExtension = ' + this.videoExtension);
+         break;
+        }
+        case MODE_SETTING.TRIGGER_FOCUS: {
+          console.log('Radio button FOCUS TRIGGER selected');
+          this.radioClickedFocus = true;
+          break;
+        }
+        default: { 
+          break; 
+        } 
+      }
+    }
+        
+    public setSensitivityThreshold(event) {
+      console.log('Sensitivity Threshold set to ' + this.sensitivity);
+    }
+
+    public setTimeBetweenTriggers(event) {
+     console.log('Time between triggers set to ' + this.triggerGap); 
     }
     
-    // To grey out modes which are not selected
-    onButtonClickDisable(event) {
-      this.buttonColor = "light";
+    public activateFocus(event) {
+      //pnarasim : make sure html and the value of this var are sync-ed! tbd
+      if (!this.focusActivated) {
+        console.log('Focus activated');
+        this.focusActivated = true;
+      } else {
+        console.log('Focus de-activated');
+        this.focusActivated = false;
+      }
     }
-    
-    sensitivityThreshold(event) {
-      
-    }
-    
-    triggerTime(event) {
-      
-    }
-    
-    activate(event) {
-      
-    }
-    
-    cameraDetails(event) {
-      
-    }
-    
-    // To initialize company names for camera model attached
-    initializeCompany() {
-      this.companies = [
+
+    // To initialize make names for camera model attached
+    initializeMakes() {
+      this.makes = [
         { id: 1, name: 'Canon' },
         { id: 2, name: 'Nikon' },
         { id: 3, name: 'Sony' }
@@ -190,79 +246,113 @@ export class DetailPage {
     }
     
     // To initialize model names for camera model attached
-    initializeModel() {
+    initializeModels() {
       this.models = [
-        { id: 1, name: 'Canon EOS 80D', company_id: 1, company_name: 'Canon' },
-        { id: 2, name: 'Canon PowerShot ELPH 190 IS', company_id: 1, company_name: 'Canon' },
-        { id: 3, name: 'Nikon D5100', company_id: 2, company_name: 'Nikon' },
-        { id: 4, name: 'Nikon D5200', company_id: 2, company_name: 'Nikon' },
-        { id: 5, name: 'Sony Alpha 580', company_id: 3, company_name: 'Sony' },
-        { id: 7, name: 'Sony Alpha 33', company_id: 3, company_name: 'Sony' }
+        { id: 1, name: 'Canon EOS 80D', make_id: 1, make_name: 'Canon' },
+        { id: 2, name: 'Canon PowerShot ELPH 190 IS', make_id: 1, make_name: 'Canon' },
+        { id: 3, name: 'Nikon D5100', make_id: 2, make_name: 'Nikon' },
+        { id: 4, name: 'Nikon D5200', make_id: 2, make_name: 'Nikon' },
+        { id: 5, name: 'Sony Alpha 580', make_id: 3, make_name: 'Sony' },
+        { id: 7, name: 'Sony Alpha 33', make_id: 3, make_name: 'Sony' }
       ];
     }
     
-    // To populate model names based on selected company name
-    setModelNames(company) {
-      this.selectedModels = this.models.filter(model => model.company_id == company.id)
+    // To populate model names based on selected make name
+    setModelNames(make) {
+      this.selectedModels = this.models.filter(model => model.make_id == make.id)
     }
+
+    public print_settings_arraybufffer(writeBuffer:ArrayBuffer) {
+
+      var dataview = new DataView(writeBuffer);
+
+      console.log('timeSetting = ' + dataview.getUint8(OFFSET_TIME));
+      console.log('mode = ' + dataview.getUint8(OFFSET_MODE));
+      console.log('burstGap | bulbExposureTime = ' + dataview.getUint16(OFFSET_BURSTGAP_BULBEXP));
+      console.log('videoDuration:videoExtension = ' + dataview.getUint8(OFFSET_VIDEO));
+      console.log('sensitivity = ' + dataview.getUint8(OFFSET_SENSITIVITY));
+      console.log('triggerGap = ' + dataview.getUint16(OFFSET_TRIGGER_GAP));
+      console.log('focusActivated = ' + dataview.getUint8(OFFSET_FOCUS_ACTIVATED));
+      console.log('make = ' + dataview.getUint8(OFFSET_MAKE));
+      console.log('model = ' + dataview.getUint8(OFFSET_MODEL));
+    }
+
+    
+    public constructArrayBufferToWrite():ArrayBuffer {
+      /*
+        Format of ArrayBuffer that the board expects : make this fw version dependent next
+        uint8 timeSetting
+        unint32 mode[
+          uint8 single|burst|bulbexp|video|focus
+          uint16 burstGap|bulbExposureTime
+          uint8 videoDuration:4, videoExtension:4
+        ] 
+        uint8 sensitivity
+        uint16 triggerGaps
+        uint8 focusActivated
+        uint8 make
+        uint8 model
+      */
+
+      let writeBuffer = new ArrayBuffer(SENSEPI_SETTINGS_LENGTH);
+      var dataview = new DataView(writeBuffer);
+      //start writing the values
+      dataview.setUint8(OFFSET_TIME,this.timeSetting);
+      dataview.setUint8(OFFSET_MODE,this.mode);
+      
+      switch (+this.mode) {
+        case MODE_SETTING.TRIGGER_SINGLE: {
+          //no extra data to record.
+          dataview.setUint16(OFFSET_BURSTGAP_BULBEXP, 0);
+          dataview.setUint8(OFFSET_VIDEO, 0);
+          break;
+        }
+        case MODE_SETTING.TRIGGER_BURST: {
+          dataview.setUint16(OFFSET_BURSTGAP_BULBEXP, this.burstGap);
+          dataview.setUint8(OFFSET_VIDEO, 0);
+          break;
+        }
+        case MODE_SETTING.TRIGGER_BULB_EXPOSURE: {
+          dataview.setUint16(OFFSET_BURSTGAP_BULBEXP, this.bulbExposureTime);
+          dataview.setUint8(OFFSET_VIDEO, 0);
+          break;
+        }
+        case MODE_SETTING.TRIGGER_VIDEO: {
+          var viddata  = this.videoDuration << 4;
+          viddata |= this.videoExtension;
+          console.log("Combining " + this.videoDuration + " and " + this.videoExtension + " gives " + viddata);
+          dataview.setUint16(OFFSET_BURSTGAP_BULBEXP, 0);
+          dataview.setUint8(OFFSET_VIDEO, viddata); 
+          break;
+        }
+        default: {
+          break;
+        }
+      } 
+      dataview.setUint8(OFFSET_SENSITIVITY, this.sensitivity);
+      dataview.setUint16(OFFSET_TRIGGER_GAP, this.triggerGap); 
+      dataview.setUint8(OFFSET_FOCUS_ACTIVATED, +this.focusActivated);
+      dataview.setUint8(OFFSET_MAKE, this.make); 
+      dataview.setUint8(OFFSET_MODEL, this.model);
+ 
+      this.print_settings_arraybufffer(writeBuffer);
+
+      return writeBuffer;
+    }
+
     
     // To write the value of each characteristic to the device 
     onButtonClickWrite(event) {
-      
-      this.ble.write(this.peripheral.id, UUID_SENSE_PI_SERVICE, UUID_SENSE_PI_TIME, this.timeSetting.buffer).then(
-        () => this.setStatus('Successfully Set timeSetting on device to ' + this.timeSetting) 
-      )
-      .catch(
-        e => console.log(e),
-      );
-      
-      
-/*
-      let modeBuffer = new Uint8Array([this.mode]).buffer;
-      this.ble.write(this.peripheral.id, APPIKOSENSE_SERVICE, APPIKOSENSE_MODE, modeBuffer).then(
-        () => this.setStatus('Set mode to ' + this.mode)  
-      )
-      .catch(
-        e => console.log(e)
-      );
-      
 
-      let sensitivityBuffer = new Uint8Array([this.sensitivity]).buffer;
-      this.ble.write(this.peripheral.id, APPIKOSENSE_SERVICE, APPIKOSENSE_SENSITIVITY, sensitivityBuffer).then(
-        () => this.setStatus('Set sensitivity to ' + this.sensitivity) 
+      let data = this.constructArrayBufferToWrite();      
+      this.ble.write(this.peripheral.id, UUID_SENSE_PI_SERVICE, UUID_SENSE_PI_USER_SETTINGS, data).then(
+        () => this.setStatus('Write Success'),
+        //console.log('Wrote all settings to the device = ' + data)
       )
       .catch(
-        e => console.log(e)
+        e => console.log('eror in writing to device : ' + e),
       );
-      
-
-      let triggersBuffer = new Uint8Array([this.triggers]).buffer;
-      this.ble.write(this.peripheral.id, APPIKOSENSE_SERVICE, APPIKOSENSE_TRIGGERS, triggersBuffer).then(
-        () => this.setStatus('Set time between triggers to ' + this.triggers)  
-      )
-      .catch(
-        e => console.log(e)
-      );
-      
-
-      let activateBuffer = new Uint8Array([this.isActivated]).buffer;
-      this.ble.write(this.peripheral.id, APPIKOSENSE_SERVICE, APPIKOSENSE_ACTIVATE, activateBuffer).then(
-        () => this.setStatus('Set signal status to ' + this.isActivated) 
-      )
-      .catch(
-        e => console.log(e)
-      );
-      
-
-      let cameraBuffer = new Uint8Array([this.model]).buffer;
-      this.ble.write(this.peripheral.id, APPIKOSENSE_SERVICE, APPIKOSENSE_CAMERA, cameraBuffer).then(
-        () => this.setStatus('Set camera model attached to ' + this.model) 
-      )
-      .catch(
-        e => console.log(e)
-      );
-    */
-
+       
     }
   
     
